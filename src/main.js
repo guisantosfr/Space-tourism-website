c4b263a045c1db1d69bdfce48c15b60796350bbf
Guilherme Santos <santosgui678@gmail.com>
2022-11-01 10:37:00 -0300

    Reset and initial configs

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
 
-import './assets/main.css'
+import App from './App.vue';
+import router from './router';
+
+import './assets/index.css'
 
 const app = createApp(App)
 
-app.use(router)
-
-app.mount('#app')
+app.use(router).mount('#app')
