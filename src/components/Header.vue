fc73a56df0fe4eb404f6f1c4c4e89643a30448e3
Guilherme Santos <santosgui678@gmail.com>
2022-11-03 10:23:52 -0300

    Make header and grid on home

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="primary-header flex">
+    <div>
+      <img src="../assets/shared/logo.svg" alt="space tourism logo" class="logo">
+    </div>
+
+    <nav>
+      <ul class="primary-navigation underline-indicators flex">
+        <li class="active">
+          <router-link class="font-sans-cond uppercase text-white letter-spacing-2" to="/">
+            <span>00</span> Home
+          </router-link>
+        </li>
+
+        <li>
+          <router-link class="font-sans-cond uppercase text-white letter-spacing-2" to="#">
+            <span>01</span> Destination
+          </router-link>
+        </li>
+
+        <li>
+          <router-link class="font-sans-cond uppercase text-white letter-spacing-2" to="#">
+            <span>02</span> Crew
+          </router-link>
+        </li>
+
+        <li>
+          <router-link class="font-sans-cond uppercase text-white letter-spacing-2" to="#">
+            <span>03</span> Technology
+          </router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
-<script setup></script>
+<script setup>
+import Header from '../components/Header.vue';
+</script>
 
 <template>
-  <h1 class="text-dark">Hello World!</h1>
+  <Header />
+
+  <div class="grid-container">
+    <div>
+      <h1 class="text-accent font-500 font-sans-cond uppercase letter-spacing-1">So, you want to travel to
+        <span class="font-900 font-serif text-white">Space</span>
+      </h1>
+
+      <p>Let’s face it; if you want to go to space, you might as well genuinely go to
+        outer space and not hover kind of on the edge of it. Well sit back, and relax
+        because we’ll give you a truly out of this world experience!</p>
+    </div>
+
+    <router-link to="#" class="large-button uppercase font-serif font-600 text-dark bg-white">Explore</router-link>
+  </div>
 </template>
